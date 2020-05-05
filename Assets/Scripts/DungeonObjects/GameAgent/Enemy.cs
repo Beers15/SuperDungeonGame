83eaa266a42b59caf287b58e19a38329ba8d5981
Alexander Beers <alexbeerz@yahoo.com>
2020-05-04 20:14:38 -0700

    Finish random item seeding, all players have synchronous item drops. (Collaborator MooOfDoom)

--- Assets/Scripts/DungeonObjects/GameAgent/Enemy.cs
+++ Assets/Scripts/DungeonObjects/GameAgent/Enemy.cs
@@ -194,11 +194,11 @@ public class Enemy : GameAgent
 				if(mobDifficultyModifier <= 0.1f)
 					lootRoll = lootThreshold + 1.0f;
 				else
-					lootRoll = Settings.globalRNG.Next(1.0f, 100.0f * (mobDifficultyModifier));
+					lootRoll = Settings.globalRNG.Next(1, (int)(100 * (mobDifficultyModifier)));
 					Debug.Log("Loot roll is " + lootRoll + " (>" + lootThreshold + " equals item drop) ");
 
 				if(lootRoll > lootThreshold) {
-					randomItem = Settings.globalRNG.Next(0, lootDrops.Length);
+					randomItem = Settings.globalRNG.Next(0, lootDrops.Length - 1);
 
 					var spawnedDrop = map_manager.instantiate_environment(lootDrops[randomItem], new Pos(this.grid_pos.x, this.grid_pos.y), true, true);
 					if(randomItem == 0) {
