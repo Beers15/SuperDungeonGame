f0d063197679d695cb211ad5e56cd719c0a05d37
adamjbea <45697567+adamjbea@users.noreply.github.com>
2020-05-11 23:43:15 -0700

    Added Item Images to EquipUI
    EquipUI will now register the correct itemImage sprite when an item is equipped. This item is then displayed in the equipUI.

---
+++ Assets/Scripts/UI Scripts/UI_EquipmentDisplay.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_EquipmentDisplay : MonoBehaviour
+{
+	public UI_EquipmentSlot HelmetSlot, ChestSlot, HandSlot, FootSlot, WeaponSlot;
+	public UI_ToolTipDisplay tooltipDisplay;
+	GameObject display;
+	public GameObject tooltipScreen;
+	Player playerMain;
+	public int selectedSlotIndex = -1;
+
+	// Start is called before the first frame update
+	void Awake()
+	{
+		
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (playerMain == null)
+		{
+			playerMain = Network.getPlayer(NetworkManager.clientID);
+			return;
+		}
+		if (playerMain.inventory.helmet != null)
+		{
+			HelmetSlot.SetItem(playerMain.inventory.helmet);
+
+		}else if(playerMain.inventory.armor != null)
+		{
+			ChestSlot.SetItem(playerMain.inventory.armor);
+
+		}else if(playerMain.inventory.gloves != null)
+		{
+			HandSlot.SetItem(playerMain.inventory.gloves);
+
+		}else if(playerMain.inventory.boots != null){
+
+			FootSlot.SetItem(playerMain.inventory.boots);
+
+		}else if(playerMain.inventory.weapon != null)
+		{
+			WeaponSlot.SetItem(playerMain.inventory.weapon);
+		}
+	}
+
+	public void TriggerSlot(int index)
+	{
+		selectedSlotIndex = index;
+		Debug.Log("shit has been triggered, index: " + index);
+		tooltipScreen.SetActive(true);
+
+	}
+
+	public void DropItem()
+	{
+		if (selectedSlotIndex == -1) return;
+		playerMain.inventory.DecrementItemAtSlot(selectedSlotIndex);
+	}
+
+	public void UseItem()
+	{
+		if (selectedSlotIndex == -1) return;
+		Network.submitCommand(new UseItemCommand(NetworkManager.clientID, selectedSlotIndex));
+	}
+
+	public void Back()
+	{
+		display.SetActive(false);
+		tooltipScreen.SetActive(false);
+	}
+
+	public int getSlot()
+	{
+		return this.selectedSlotIndex;
+	}
+}
---
+++ Assets/Scripts/UI Scripts/UI_EquipmentSlot.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_EquipmentSlot : MonoBehaviour
+{
+	public Image itemImage;
+	public Item slotItem;
+	public string itemName;
+	UI_EquipmentDisplay manager;
+
+	public Sprite bow, sword, staff, helmet, armor, gloves, boots;
+
+	public int index = 0;
+
+	void Awake()
+	{
+	}
+
+	public void TriggerSlot()
+	{
+		manager.TriggerSlot(this.index);
+	}
+
+	public void SetItem(Item item)
+	{
+		if (item == null) return;
+
+		slotItem = item;
+		itemImage.color = new Color(1, 1, 1, 1);
+
+		itemName = item.name;
+		//	Debug.Log("IN INVENTORY SLOT THE SPRITE NAME IS " +item.name);
+		switch (item.name)
+		{
+			case "Helmet":
+				itemImage.sprite = helmet; break;
+			case "Armor":
+				itemImage.sprite = armor; break;
+			case "Gloves":
+				itemImage.sprite = gloves; break;
+			case "Boots":
+				itemImage.sprite = boots; break;
+			case "Bow":
+				itemImage.sprite = bow; break;
+			case "Sword":
+				itemImage.sprite = sword; break;
+			case "Staff":
+				itemImage.sprite = staff; break;
+		}
+	}
+
+	public Sprite getImage()
+	{
+		return this.itemImage.sprite;
+	}
+
+	public Item getItem()
+	{
+		return this.slotItem;
+	}
+}
